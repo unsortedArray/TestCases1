a79540692dcbc6a9adf94badaa3c89d9acc3916c
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-07 03:46:09 +0530

    Initial commit #2

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 
 const should = chai.should();
 chai.use(chaiHttp);
-let server = require('../Node-Evaluation/index');
+let server = require('../index.js');
 
 describe('GeT', () =>{
     it('should test git pages', (done) =>{
