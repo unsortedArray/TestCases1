ae7a03b09d9b4d7515bfe20456f922b24e016d75
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-07 03:33:04 +0530

    moving test cases

---
+++ test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+chai.use(chaiHttp);
+let server = require('../Node-Evaluation/index');
+
+describe('GeT', () =>{
+    it('should test git pages', (done) =>{
+        chai.request(server).get('/').end((err,res)=>{
+            res.should.have.status(200);
+            done();
+        })
+    })
+})
